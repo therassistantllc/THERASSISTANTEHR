379a286561827b3c4e57016ab5a6f2435b042389
therassistantllc <admin@therassistant.com>
2026-05-27 02:26:16 +0000

    fix: Import useEffect to manage component lifecycle in AppSidebarNav

--- artifacts/therassistant-ehr/components/layout/AppSidebarNav.tsx
+++ artifacts/therassistant-ehr/components/layout/AppSidebarNav.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./AppShell.module.css";
 
 function CalendarIcon() {
